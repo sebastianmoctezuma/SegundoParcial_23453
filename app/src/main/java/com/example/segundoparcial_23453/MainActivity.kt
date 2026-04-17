dd038f56e7f363f9c9b852625bb89ee58fbf6c8b
Sebastian Moctezuma Toral <sebastianmoctezumatoral@MacBook-Air-de-Sebastian.local>
2026-04-17 17:20:32 -0600

    Primer commit

---
+++ app/src/main/java/com/example/segundoparcial_23453/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.segundoparcial_23453
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.segundoparcial_23453.ui.theme.SegundoParcial_23453Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SegundoParcial_23453Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SegundoParcial_23453Theme {
+        Greeting("Android")
+    }
+}
